a0773de32e3979796c8141096bfe2eb1e15a3432
GZ <949404079@qq.com>
2021-04-10 09:22:53 +0800

    :package: style: XMLHttpRequestResponseType

--- src/common/util/config.ts
+++ src/common/util/config.ts
@@ -1,4 +1,6 @@
+// import { XMLHttpRequestResponseType } from 'typescript'
 import { createFromIconfontCN } from '@ant-design/icons'
+import moment from 'moment'
 import { createBrowserHistory, createMemoryHistory } from 'history'
 
 interface IUser {
@@ -43,6 +45,58 @@ const treeDataFormate = (data: any, fn: any, saveCall: any) => {
   )
 }
 
+/*
+ * 根据后端返回的pdf文件的地址，下载pdf文件
+ *  url 完整的路径
+ *  fileName 文件名
+ * type 文件类型，如.pdf
+ */
+const downFile = (url: string, fileName: string, type: string) => {
+  let reg = /^([hH][tT]{2}[pP]:\/\/|[hH][tT]{2}[pP][sS]:\/\/)(([A-Za-z0-9-~]+).)+([A-Za-z0-9-~/])+$/
+  if (!reg.test(url)) {
+    throw new Error('传入参数不合法,不是标准的文件链接')
+  } else {
+    let xhr = new XMLHttpRequest()
+    xhr.open('get', url, true)
+    xhr.setRequestHeader('Content-Type', `application/${type}`)
+    xhr.responseType = 'blob'
+    xhr.onload = function () {
+      if (this.status == 200) {
+        // 接受二进制文件流
+        let blob = this.response
+        downloadExportFile(blob, fileName, type)
+      }
+    }
+    xhr.send()
+  }
+}
+
+const downloadExportFile = (
+  blob: string | XMLHttpRequestResponseType,
+  tagFileName: string,
+  fileType: string
+) => {
+  let downloadElement = document.createElement('a')
+  let href = blob
+  if (typeof blob == 'string') {
+    downloadElement.target = '_blank'
+  } else {
+    href = window.URL.createObjectURL(blob) // 创建下载的链接
+  }
+  downloadElement.href = href
+  downloadElement.download =
+    tagFileName +
+    moment(new Date().getTime()).format('YYYYMMDDhhmmss') +
+    '.' +
+    fileType // 下载后文件名
+  document.body.appendChild(downloadElement)
+  downloadElement.click() // 点击下载
+  document.body.removeChild(downloadElement) // 下载完成移除元素
+  if (typeof blob != 'string') {
+    window.URL.revokeObjectURL(href) // 释放掉blob对象
+  }
+}
+
 const config = {
   development: {
     url: 'https://a.com/'
@@ -57,4 +111,4 @@ const config = {
     url: 'https://d.com/'
   }
 }
-export { IconfontCN, history, treeDataFormate, config }
+export { IconfontCN, history, treeDataFormate, config, downFile }
